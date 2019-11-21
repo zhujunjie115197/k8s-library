0a74fc12988d56f684ee840a73ad2e470eb68180
zhujunjie <1151979400@qq.com>
2019-11-21 10:47:52 +0800

    api-server Dockerfile commit

---
+++ pull_k8s_images.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+# Check version in https://kubernetes.io/docs/reference/setup-tools/kubeadm/kubeadm-init/
+# Search "Running kubeadm without an internet connection"
+# For running kubeadm without an internet connection you have to pre-pull the required master images for the version of choice:
+KUBE_VERSION=v1.15.0
+KUBE_PAUSE_VERSION=3.1
+ETCD_VERSION=3.3.10
+DNS_VERSION=1.3.1
+
+GCR_URL=k8s.gcr.io
+ALIYUN_URL=registry.cn-shenzhen.aliyuncs.com/cookcodeblog
+
+images=(kube-proxy:${KUBE_VERSION}
+kube-scheduler:${KUBE_VERSION}
+kube-controller-manager:${KUBE_VERSION}
+kube-apiserver:${KUBE_VERSION}
+pause:${KUBE_PAUSE_VERSION}
+etcd:${ETCD_VERSION}
+coredns:${DNS_VERSION}
+)
+
+
+for imageName in ${images[@]} ; do
+  docker pull $ALIYUN_URL/$imageName
+  docker tag  $ALIYUN_URL/$imageName $GCR_URL/$imageName
+  docker rmi $ALIYUN_URL/$imageName
+done
+
+docker images
